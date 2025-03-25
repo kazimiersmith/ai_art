15e4d3c0ce90c60b4b0ea99ff8f7ef7ea59fcdc9
Kazimier Smith <kas1112@stern.nyu.edu>
2025-03-25 08:45:23 -0400

    Fix regressions to use weekly data

--- code/r/regressions.R
+++ code/r/regressions.R
@@ -7,29 +7,29 @@ data <- paste0(root, '/data')
 tab <- paste0(root, '/tab')
 
 pd <- import('pandas')
-panel <- pd$read_pickle(paste0(data, '/daily_data.pkl'))
-panel_both <- pd$read_pickle(paste0(data, '/daily_data_both.pkl'))
+panel <- pd$read_pickle(paste0(data, '/freq_data.pkl'))
+panel_both <- pd$read_pickle(paste0(data, '/freq_data_both.pkl'))
 
 # Exclude dates before all AI art subreddits existed
 panel <- panel[panel$date >= '2022-07-23', ]
 panel_both <- panel_both[panel_both$date >= '2022-07-23', ]
 
 # Posts
-reg1 <- lm(posts_organic ~ 0 + posts_ai + factor(day_of_week), data = panel)
-reg2 <- lm(posts_per_author_organic ~ 0 + posts_per_author_ai + factor(day_of_week), data = panel)
-reg3 <- lm(authors_organic ~ 0 + authors_ai + factor(day_of_week), data = panel)
-reg4 <- lm(post_score_organic ~ 0 + post_score_ai + factor(day_of_week), data = panel)
+reg1 <- lm(posts_organic ~ 0 + posts_ai, data = panel)
+reg2 <- lm(posts_per_author_organic ~ 0 + posts_per_author_ai, data = panel)
+reg3 <- lm(authors_organic ~ 0 + authors_ai, data = panel)
+reg4 <- lm(post_score_organic ~ 0 + post_score_ai, data = panel)
 
 # Comments
-reg5 <- lm(comments_organic ~ 0 + comments_ai + factor(day_of_week), data = panel)
-reg6 <- lm(comments_per_author_organic ~ 0 + comments_per_author_ai + factor(day_of_week), data = panel)
-reg7 <- lm(commenters_organic ~ 0 + commenters_ai + factor(day_of_week), data = panel)
-reg8 <- lm(comments_per_post_organic ~ 0 + comments_per_post_ai + factor(day_of_week), data = panel)
+reg5 <- lm(comments_organic ~ 0 + comments_ai, data = panel)
+reg6 <- lm(comments_per_author_organic ~ 0 + comments_per_author_ai, data = panel)
+reg7 <- lm(commenters_organic ~ 0 + commenters_ai, data = panel)
+reg8 <- lm(comments_per_post_organic ~ 0 + comments_per_post_ai, data = panel)
 
-reg9 <- lm(comments_organic ~ 0 + comments_ai + factor(day_of_week), data = panel_both)
-reg10 <- lm(comments_per_author_organic ~ 0 + comments_per_author_ai + factor(day_of_week), data = panel_both)
-reg11 <- lm(commenters_organic ~ 0 + commenters_ai + factor(day_of_week), data = panel_both)
-reg12 <- lm(comments_per_post_organic ~ 0 + comments_per_post_ai + factor(day_of_week), data = panel_both)
+reg9 <- lm(comments_organic ~ 0 + comments_ai, data = panel_both)
+reg10 <- lm(comments_per_author_organic ~ 0 + comments_per_author_ai, data = panel_both)
+reg11 <- lm(commenters_organic ~ 0 + commenters_ai, data = panel_both)
+reg12 <- lm(comments_per_post_organic ~ 0 + comments_per_post_ai, data = panel_both)
 
 out = stargazer(reg1, reg2, reg3, reg4,
 				float = FALSE,
@@ -42,8 +42,7 @@ out = stargazer(reg1, reg2, reg3, reg4,
 								 c('Mean dep. var.', round(mean(na.omit(panel$posts_organic)), digits = 4),
 								   round(mean(na.omit(panel$posts_per_author_organic)), digits = 4),
 								   round(mean(na.omit(panel$authors_organic)), digits = 4),
-								   round(mean(na.omit(panel$post_score_organic)), digits = 4)),
-								 c('Day of week FE', 'Yes', 'Yes', 'Yes', 'Yes')
+								   round(mean(na.omit(panel$post_score_organic)), digits = 4))
 								 ),
 				dep.var.labels = c('Posts (org.)',
 								   'Posts per author (org.)',
@@ -71,8 +70,7 @@ out = stargazer(reg5, reg6, reg7, reg8,
 								 c('Mean dep. var.', round(mean(na.omit(panel$comments_organic)), digits = 4),
 								   round(mean(na.omit(panel$comments_per_author_organic)), digits = 4),
 								   round(mean(na.omit(panel$commenters_organic)), digits = 4),
-								   round(mean(na.omit(panel$comments_per_post_organic)), digits = 4)),
-								 c('Day of week FE', 'Yes', 'Yes', 'Yes', 'Yes')
+								   round(mean(na.omit(panel$comments_per_post_organic)), digits = 4))
 								 ),
 				dep.var.labels = c('Comments (org.)',
 								   'Comments per author (org.)',
